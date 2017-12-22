ef85f6606bfb5fa63cc251dd6d3e6cfe12dee509
RedRogue011 <a.bronzzan@gmail.com>
2017-12-21 23:28:21 -0500

    lab 11-ab

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function button1(id) { // Hide
+    document.getElementById(id).style.display = "none";
+}
+
+function button2(id) { // Show
+    document.getElementById(id).style.display = "block";
+}
